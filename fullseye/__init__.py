4ca23bc0e1c99f3f690405ce898122f5348bcd31
furuse-kazufumi <kazufumi@furuse.work>
2026-08-13 08:56:19 +0900

    auto: pyproject.toml 編集前 (2026-08-13 08:56)

--- fullseye/__init__.py
+++ fullseye/__init__.py
@@ -46,4 +46,5 @@ __all__ = [
     "colorize_disparity", "colorize_labels", "overlay_mask", "save", "load", "save_ply",
     "detect", "segment_objects", "object_descriptor", "nearest_prototype", "draw_objects",
     "registration", "kabsch", "icp", "apply_transform",
+    "pose", "pose_descriptor", "skeleton_nodes", "principal_axis",
 ]
