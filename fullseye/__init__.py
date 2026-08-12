daabc804574ddc7e327b844815af1d21ed4262d9
furuse-kazufumi <kazufumi@furuse.work>
2026-08-13 08:52:20 +0900

    auto: pyproject.toml 編集前 (2026-08-13 08:52)

--- fullseye/__init__.py
+++ fullseye/__init__.py
@@ -44,4 +44,5 @@ __all__ = [
     "imgio", "to_float01", "to_uint8", "apply_cmap", "colorize_depth",
     "colorize_disparity", "colorize_labels", "overlay_mask", "save", "load", "save_ply",
     "detect", "segment_objects", "object_descriptor", "nearest_prototype", "draw_objects",
+    "registration", "kabsch", "icp", "apply_transform",
 ]
