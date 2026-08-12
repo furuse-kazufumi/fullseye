a19ab6c0bd1fa706e263014b07dae30264c0d5c1
furuse-kazufumi <kazufumi@furuse.work>
2026-08-13 08:56:10 +0900

    auto: __init__.py 編集前 (2026-08-13 08:56)

--- api.py
+++ api.py
@@ -62,6 +62,7 @@ __all__ = [
     "colorize_disparity", "colorize_labels", "overlay_mask", "save", "load", "save_ply",
     "detect", "segment_objects", "object_descriptor", "nearest_prototype", "draw_objects",
     "registration", "kabsch", "icp", "apply_transform",
+    "pose", "pose_descriptor", "skeleton_nodes", "principal_axis",
 ]
 
 __version__ = "0.1.0"
