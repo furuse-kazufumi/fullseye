47a3b175182b0b566754bcc95e6bd2dbe0cf681d
furuse-kazufumi <kazufumi@furuse.work>
2026-08-13 20:36:35 +0900

    auto: test_flow.py 編集前 (2026-08-13 20:36)

--- fullseye/__init__.py
+++ fullseye/__init__.py
@@ -65,6 +65,6 @@ __all__ = [
     "flow", "optical_flow_lk", "optical_flow_hs", "warp_by_flow",
     "flow_magnitude", "flow_angle",
     "motion", "frame_motion_energy", "dominant_motion", "flow_from_model",
-    "residual_motion", "motion_segments",
+    "residual_motion", "motion_segments", "motion_energy_series", "detect_events",
     "recipes", "recipe", "measure", "line_profile", "distance", "angle",
 ]
