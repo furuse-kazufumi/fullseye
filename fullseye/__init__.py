69d97c1d82b8dfdf7df9dac78c12ae8b70c24313
furuse-kazufumi <kazufumi@furuse.work>
2026-08-13 19:41:34 +0900

    auto: __init__.py 編集前 (2026-08-13 19:41)

--- fullseye/__init__.py
+++ fullseye/__init__.py
@@ -28,7 +28,8 @@ with warnings.catch_warnings():
     from api import (  # noqa: E402,F401
         apply, run_pipeline, find_op, list_ops, op_names, categories,
         read_image, write_image, RT, REGISTRY, __version__, version,
-        stereo, disparity_map, depth_from_disparity, reproject_to_points,
+        stereo, disparity_map, disparity_subpixel, lr_consistency,
+        depth_from_disparity, reproject_to_points,
         terrain, elevation_map, traversability, foothold_score, fill_gaps,
         imgio, to_float01, to_uint8, apply_cmap, colorize_depth,
         colorize_disparity, colorize_labels, colorize_height, colorize_flow,
