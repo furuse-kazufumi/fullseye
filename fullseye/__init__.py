c618f88c546a826cdfacb3459786dcccd62f4c37
furuse-kazufumi <kazufumi@furuse.work>
2026-08-13 22:13:03 +0900

    auto: perception_on_video.py 編集前 (2026-08-13 22:13)

--- fullseye/__init__.py
+++ fullseye/__init__.py
@@ -69,6 +69,6 @@ __all__ = [
     "flow_magnitude", "flow_angle", "track_points",
     "motion", "frame_motion_energy", "dominant_motion", "flow_from_model",
     "residual_motion", "motion_segments", "motion_energy_series", "detect_events",
-    "video", "read_frames", "iter_frames", "frame_pairs", "write_video",
+    "video", "read_frames", "iter_frames", "frame_pairs", "write_video", "probe",
     "recipes", "recipe", "measure", "line_profile", "distance", "angle",
 ]
