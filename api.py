8c4a282e71c3e839f926b6e0d2c954024e0197ad
furuse-kazufumi <kazufumi@furuse.work>
2026-08-16 16:22:59 +0900

    auto: __init__.py 編集前 (2026-08-16 16:22)

--- api.py
+++ api.py
@@ -280,6 +280,8 @@ __all__ = [
     "fit_line", "fit_circle", "fit_ellipse", "fit_rectangle2",
     "algo", "algo_ops", "algo_categories", "find_algo", "run_algo",
     "algo_to_python", "algo_to_c", "algo_difftest",
+    "synth", "learn_features", "synthesize_like", "match_histogram",
+    "radial_power_spectrum", "feature_distance", "patch_novelty",
 ]
 
 
