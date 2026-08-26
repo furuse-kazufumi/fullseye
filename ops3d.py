84e3d13632fcf26184d87dca72181b1506b110ac
furuse-kazufumi <kazufumi@furuse.work>
2026-08-27 08:26:52 +0900

    auto: ops3d.py 編集前 (2026-08-27 08:26)

--- ops3d.py
+++ ops3d.py
@@ -16,9 +16,17 @@ import feat_spin
 import feat_shot
 import feat_fpfh
 import fuse3d
+import photometric
+import range_image
+import pcl_filter
+import fringe
+import deform3d
+import medial
 
 _MOD = {"match3d": match3d, "feat_harris": feat_harris, "feat_spin": feat_spin,
-        "feat_shot": feat_shot, "feat_fpfh": feat_fpfh, "fuse3d": fuse3d}
+        "feat_shot": feat_shot, "feat_fpfh": feat_fpfh, "fuse3d": fuse3d,
+        "photometric": photometric, "range_image": range_image, "pcl_filter": pcl_filter,
+        "fringe": fringe, "deform3d": deform3d, "medial": medial}
 
 # 入出力の「種別」語彙(op 連結の型検査に使う):
 #   voxel / points / mesh / depth / sdf / normals / gaussians / image2d /
