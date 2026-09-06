297a1eefe4b68d248f2a631d3aba44cec8c920ca
furuse-kazufumi <kazufumi@furuse.work>
2026-09-06 21:16:12 +0900

    auto: poc_registration_basin.py 編集前 (2026-09-06 21:16)

--- examples/poc_particle_tracking.py
+++ examples/poc_particle_tracking.py
@@ -706,7 +706,7 @@ def section9_findings(rec_density, got_peaks):
   (6) ★★時空間の 3-D 局所極大は per-frame 検出の代わりにならない
       (min_distance=1 で真値の %.0f %%)。等方近傍は時間軸に合わない。
 """ % (rec_density["d_det_nn"][-1], rec_density["d_tru_nn"][-1],
-       rec_density["drift_det_nn"][-1], 100 * got_peaks[1] / (T * 100.0)))
+       rec_density["drift_det_nn"][-1], 100 * got_peaks[1][2]))
 
 
 def section10_tool_gaps():
