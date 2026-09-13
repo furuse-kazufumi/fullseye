04d8bdaaa25313ec54c1ce1f3d9557cd4eb36651
furuse-kazufumi <kazufumi@furuse.work>
2026-09-13 22:51:24 +0900

    auto: opdocs.py 編集前 (2026-09-13 22:51)

--- opassist.py
+++ opassist.py
@@ -35,7 +35,7 @@ _LEDGERS = (
     ("opsspecular", "OPSSPECULAR"), ("opsmotionmag", "OPSMOTIONMAG"),
     ("opsquat", "OPSQUAT"), ("opsrangedoppler", "OPSRANGEDOPPLER"),
     ("opsacoustics", "OPSACOUSTICS"), ("opsinterferometry", "OPSINTERFEROMETRY"),
-    ("opsflyvision", "OPSFLYVISION"),
+    ("opsflyvision", "OPSFLYVISION"), ("opsspc", "OPSSPC"),
     ("opstomography", "OPSTOMOGRAPHY"), ("opsvolcolor", "OPSVOLCOLOR"),
     ("opsreprconv", "OPSREPRCONV"), ("opscadmap", "OPSCADMAP"),
     ("opsannotate", "OPSANNOTATE"), ("opsgfx2d", "OPSGFX2D"),
