bce373d86f07a837dfda0ee94a2721c79fd436d1
furuse-kazufumi <kazufumi@furuse.work>
2026-09-13 22:50:42 +0900

    auto: typed_catalog.py 編集前 (2026-09-13 22:50)

--- typed_catalog.py
+++ typed_catalog.py
@@ -605,7 +605,7 @@ def _registry_adapters():
                  "opsgfx2d", "opsimgmetrics", "opscolortransport",
                  "opsimgforensics", "opsastrostack", "opsdem", "opspiv",
                  "opsprofile", "opsshapestat", "opsshape2d", "opsroughness",
-                 "opsmeasure1d", "opsblob", "opsflyvision"):
+                 "opsmeasure1d", "opsblob", "opsflyvision", "opsspc"):
         try:
             d.update(getattr(__import__(_mod), "RESULT_ADAPTERS", {}))
         except Exception as _e:                       # 台帳が無い環境でも動く
